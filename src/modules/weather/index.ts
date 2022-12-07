d31bb6326ff049aadc9d74c2b3d0764374fb90b5
suemor <3100825062@qq.com>
2022-12-08 00:47:43 +0800

    feat: weather forecast

--- src/modules/weather/index.ts
+++ src/modules/weather/index.ts
@@ -1,3 +1,7 @@
+import { botConfig } from 'config'
+import { CronJob } from 'cron'
+
+import { client } from '~/client'
 import { commandRegistry } from '~/registries/command'
 import { weatherRequest } from '~/service/weather'
 
@@ -11,4 +15,18 @@ export const register = () => {
       `城市: 常州\n当前气温: ${toWeather.tem}℃\n当前天气: ${toWeather.wea}\n今日气温: ${toWeather.tem2}℃ - ${toWeather.tem1}℃\n----------------\n明日天气: ${tmrWeather.wea}\n明日气温: ${tmrWeather.tem2}℃ - ${tmrWeather.tem1}℃`,
     )
   })
+
+  const weatherForecast = new CronJob('0 0 6 * * *', async () => {
+    const weather = await weatherRequest()
+    const toWeather = weather.data.data[0]
+    const tasks = botConfig.watchGroupIds.map((id) =>
+      client.sendGroupMsg(
+        id,
+        `早上好！今日天气如下:\n城市: 常州\n当前气温: ${toWeather.tem}℃\n今日天气: ${toWeather.wea}\n今日气温: ${toWeather.tem2}℃ - ${toWeather.tem1}℃`,
+      ),
+    )
+    await Promise.all(tasks)
+  })
+
+  weatherForecast.start()
 }
