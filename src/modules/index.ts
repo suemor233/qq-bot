cd4325a4eedf79612b451f70c3785875e6839b21
suemor <3100825062@qq.com>
2022-12-08 00:28:00 +0800

    style: remove invalid code

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -22,23 +22,3 @@ export const registerModules = async () => {
     }
   })
 }
-
-
-// export const registerModules = async() => {
-  
-//   client.on('system.online', () => {
-//     weatherForecast.start()
-//   })
-// }
-
-// const weatherForecast = new CronJob('0 0 6 * * *', async () => {
-//   const weather = await weatherRequest()
-//   const toWeather = weather.data.data[0]
-//   const tasks = botConfig.watchGroupIds.map((id) =>
-//     client.sendGroupMsg(
-//       id,
-//       `下午好！今日天气如下:\n城市: 常州\n当前气温: ${toWeather.tem}℃\n今日天气: ${toWeather.wea}\n今日气温: ${toWeather.tem2}℃ - ${toWeather.tem1}℃`,
-//     ),
-//   )
-//   await Promise.all(tasks)
-// })
